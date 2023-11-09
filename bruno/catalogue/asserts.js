97d0839269b97d4e6e94155cb2492d9b28f922e7
Niccolò Cantù <niccolo.cantu@nina.no>
2023-11-09 15:28:32 +0100

    improve bruno setup, add iso request from pycsw tests

---
+++ bruno/catalogue/asserts.js
@@ -0,0 +1,21 @@
+const { XMLParser, XMLBuilder, XMLValidator} = require("fast-xml-parser");
+
+function isValidXML(res) {
+    const parser = new XMLParser();
+    let jObj = parser.parse(res.getBody());
+
+    const builder = new XMLBuilder();
+    const xmlContent = builder.build(jObj);
+}
+
+function isNotAnException(res) {
+    const parser = new XMLParser();
+    let jObj = parser.parse(res.getBody());
+
+    expect(jObj).not.to.have.property('ows:ExceptionReport')
+}
+
+module.exports = {
+    isValidXML,
+    isNotAnException,
+}
--- bruno/catalogue/entrypoint.sh
+++ bruno/catalogue/entrypoint.sh
@@ -7,7 +7,7 @@ if [[ -z "${WAIT_FOR_HTTP}" ]]
 then
   echo "No HTTP service to wait for"
 else
-  ./wait-for -t 60 "$WAIT_FOR_HTTP"
+  wait-for -t 60 "$WAIT_FOR_HTTP"
 fi
 
-exec /usr/local/bin/bru run --env Docker "$@"
+exec bru run --env Docker "$@"
