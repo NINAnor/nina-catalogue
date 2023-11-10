82d6b7d3e28d1c38b94fa4121cfaa129d7b0eced
Niccolò Cantù <niccolo.cantu@nina.no>
2023-11-10 14:58:58 +0100

    handle srs, polygons, functions, filters

--- metadata_catalogue/datasets/libs/csw_query.py
+++ metadata_catalogue/datasets/libs/csw_query.py
@@ -1,48 +1,80 @@
 import operator
+import re
 from functools import reduce
 
 import pyproj
-from django.contrib.gis.geos import Polygon
+from django.contrib.gis.geos import LinearRing, Polygon
 from django.db.models import Q
 
 from .. import logger
 
 
-def bbox_to_geometry(bbox):
-    xmin, ymin = bbox["gml:lowerCorner"].split(" ")
-    xmax, ymax = bbox["gml:upperCorner"].split(" ")
-    p = Polygon.from_bbox((xmin, ymin, xmax, ymax))
-    if "@srsName" in bbox:
-        p.srid = pyproj.CRS.from_string(bbox["@srsName"]).to_epsg()
+def set_srid(gml_dict, geom):
+    if "@srsName" in gml_dict:
+        geom.srid = pyproj.CRS.from_string(gml_dict["@srsName"]).to_epsg()
     else:
-        p.srid = 4326
-    return p
+        geom.srid = 4326
 
 
-def not_implemented_search(r):
-    return Q()
+def build_linear_ring(ring):
+    coords = [float(coord) for coord in ring["gml:posList"]["#text"].split(" ")]
+    dimension = int(ring["gml:posList"]["@srsDimension"])
+    tuples = [tuple(coords[i * dimension : (i + 1) * dimension]) for i in range(int(len(coords) / dimension))]
+    return LinearRing(*tuples)
+
+
+def to_sql_like_regex(term):
+    escaped = re.escape(term)
+    return re.compile(escaped.replace(r"\?", ".").replace(r"\%", ".*"))
 
 
 class Rule:
     def __init__(self, operation, ogc_dict):
         _prefix, self.operation = operation.split(":")
         self._dict = ogc_dict
-        _prefix, self.field = ogc_dict["ogc:PropertyName"].split(":")
+        self.function = None
+        self.field = None
+        if "ogc:Function" in self._dict:
+            # TODO: implement functions
+            self.function = self._dict["ogc:Function"]["@name"]
+            _prefix, self.field = self._dict["ogc:Function"]["ogc:PropertyName"].split(":")
+        elif "ogc:PropertyName" in self._dict:
+            _prefix, self.field = self._dict["ogc:PropertyName"].split(":")
         self._get_value()
 
     def _get_value(self):
-        if self.operation == "BBOX":
-            self.value = bbox_to_geometry(self._dict["gml:Envelope"])
-        else:
+        if "gml:Envelope" in self._dict:
+            bbox = self._dict["gml:Envelope"]
+            xmin, ymin = bbox["gml:lowerCorner"].split(" ")
+            xmax, ymax = bbox["gml:upperCorner"].split(" ")
+            self.value = Polygon.from_bbox((xmin, ymin, xmax, ymax))
+            set_srid(bbox, self.value)
+        elif "gml:Polygon" in self._dict:
+            polygon = self._dict["gml:Polygon"]
+            # TODO: manage interior rings for polygon
+            self.value = Polygon(build_linear_ring(polygon["gml:exterior"]["gml:LinearRing"]))
+            set_srid(polygon, self.value)
+
+        elif "ogc:Literal" in self._dict:
             self.value = self._dict["ogc:Literal"]
+        else:
+            self.value = None
 
     IMPLEMENTATION = {
         "PropertyIsLike": {
-            "AnyText": lambda r: Q(metadata__fts__icontains=r.value),
+            "AnyText": lambda r: Q(metadata__fts__iregex=to_sql_like_regex(r.value)),
+            "Abstract": lambda r: Q(metadata__abstract__iregex=to_sql_like_regex(r.value)),
+            "Title": lambda r: Q(metadata__title__iregex=to_sql_like_regex(r.value)),
             "ServiceType": lambda r: Q() if r.value == "view" else Q(id__lt=0),
         },
         "BBOX": {
             "BoundingBox": lambda r: Q(metadata__bounding_box__within=r.value),
+        },
+        "PropertyIsBetween": {
+            "title": lambda r: Q(
+                metadata__title__lt=r._dict["ogc:UpperBoundary"]["ogc:Literal"],
+                metadata__title__gt=r._dict["ogc:LowerBoundary"]["ogc:Literal"],
+            ),
         },
     }
 
