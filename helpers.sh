5b6dba9217676fb48325f4c511824ae9028239dc
Niccolò Cantù <niccolo.cantu@nina.no>
2023-11-14 14:34:03 +0100

    fix django production build

--- helpers.sh
+++ helpers.sh
@@ -4,4 +4,4 @@ alias dpcli_dev="docker compose --profile dev --env-file secrets/docker"
 alias dpcli_prod="docker compose --profile prod --env-file secrets/docker"
 
 alias djcli_dev="docker compose --profile dev --env-file secrets/docker exec -it django-dev python manage.py"
-alias djcli_prod="docker compose --profile prod --env-file secrets/docker exec -it django-dev python manage.py"
+alias djcli_prod="docker compose --profile prod --env-file secrets/docker exec -it django python manage.py"
