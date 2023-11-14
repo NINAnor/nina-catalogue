b0f9d150bd7dc478aeffa4e77ce7e9bb41fac60a
Niccolò Cantù <niccolo.cantu@nina.no>
2023-11-14 14:58:49 +0100

    add service info to admin

--- metadata_catalogue/datasets/admin.py
+++ metadata_catalogue/datasets/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.contrib.gis.admin import GISModelAdmin
+from solo.admin import SingletonModelAdmin
 
 from metadata_catalogue.datasets import models
+
+admin.site.register(models.ServiceInfo, SingletonModelAdmin)
+
 
 admin.site.register(models.Dataset)
 admin.site.register(models.Person)
