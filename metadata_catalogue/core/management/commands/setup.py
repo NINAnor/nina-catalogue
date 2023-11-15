950c5b27e6113c4d354a381d4ca53c804f6f2f4c
Niccolò Cantù <niccolo.cantu@nina.no>
2023-11-15 13:13:46 +0100

    fallback in case of failed network connection in setup

--- metadata_catalogue/core/management/commands/setup.py
+++ metadata_catalogue/core/management/commands/setup.py
@@ -1,3 +1,5 @@
+import traceback
+
 from countries_plus.models import Country
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
@@ -12,7 +14,12 @@ class Command(BaseCommand):
             call_command("loaddata", "users.json")
 
         if Country.objects.all().first() is None:
-            call_command("update_countries_plus")
+            try:
+                call_command("update_countries_plus")
+            except:
+                print(traceback.format_exc())
+                print("Falling back to local fixture")
+            call_command("loaddata", "countries.json.gz")
 
         if Language.objects.all().first() is None:
             call_command("loaddata", "languages_data.json.gz")
