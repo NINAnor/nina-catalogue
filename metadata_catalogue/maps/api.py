5c680c10bc1edae95adc01f22ab1bff79e550e89
Niccolò Cantù <niccolo.cantu@nina.no>
2023-12-18 15:11:11 +0100

    use slug instead of uuid for maps

--- metadata_catalogue/maps/api.py
+++ metadata_catalogue/maps/api.py
@@ -1,5 +1,3 @@
-from uuid import UUID
-
 from ninja import Router
 
 from . import models, schema
@@ -9,26 +7,26 @@ maps_router = Router()
 
 # https://github.com/vitalik/django-ninja/issues/803 - by_alias is needed for dashed properties
 @maps_router.get(
-    "/maps/{map_uuid}/metadata/",
+    "/maps/{map_slug}/metadata/",
     response=schema.MapMetadata,
     url_name="map_metadata",
     by_alias=True,
     exclude_none=True,
 )
-def get_map_metadata(request, map_uuid: UUID):
+def get_map_metadata(request, map_slug: str):
     try:
-        m = models.Map.objects.get(uuid=map_uuid)
+        m = models.Map.objects.get(slug=map_slug)
         return m.get_metadata(request)
     except models.Map.DoesNotExist:
         return {}
 
 
 @maps_router.get(
-    "/maps/{map_uuid}/style/", response=schema.MapStyle, url_name="map_style", by_alias=True, exclude_none=True
+    "/maps/{map_slug}/style/", response=schema.MapStyle, url_name="map_style", by_alias=True, exclude_none=True
 )
-def get_map_style(request, map_uuid: UUID):
+def get_map_style(request, map_slug: str):
     try:
-        m = models.Map.objects.get(uuid=map_uuid)
+        m = models.Map.objects.get(slug=map_slug)
         return m.get_style(request)
     except models.Map.DoesNotExist:
         return {}
