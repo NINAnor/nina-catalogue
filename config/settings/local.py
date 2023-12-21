6949bcd596eb6374590ffbaf85b7e4b4dfa76fc8
Niccolò Cantù <niccolo.cantu@nina.no>
2023-12-21 10:23:02 +0100

    fix cors paramenters

--- config/settings/local.py
+++ config/settings/local.py
@@ -54,4 +54,6 @@ INSTALLED_APPS += ["django_extensions"]  # noqa: F405
 
 # Your stuff...
 # ------------------------------------------------------------------------------
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^http://localhost\:\d{4}$",
+]
