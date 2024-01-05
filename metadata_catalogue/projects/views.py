7c2b7f1e9e7fd83761b534f0976e18f153613a88
Niccolò Cantù <niccolo.cantu@nina.no>
2024-01-05 16:33:22 +0100

    implement basic projects support

---
+++ metadata_catalogue/projects/views.py
@@ -0,0 +1,38 @@
+import swapper
+from django.urls import reverse
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+
+Project = swapper.load_model("projects", "Project")
+
+
+class ProjectListView(ListView):
+    model = Project
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+
+
+class ProjectDeleteView(DeleteView):
+    model = Project
+
+    def get_success_url(self) -> str:
+        return reverse("projects:list")
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    fields = [
+        "name",
+        "slug",
+        "is_active",
+    ]
+    template_name_suffix = "_create_form"
+
+
+class ProjectUpdateView(UpdateView):
+    model = Project
+    fields = [
+        "name",
+        "slug",
+    ]
